9f3f987fa8d410a6f8779bcbdf3c2beb486de53d
Vitali <vliavitski@Vitalis-MacBook-Pro.local>
2021-12-10 20:58:10 +0300

    Second lesson

--- src/com/exlab/FirstLesson.java
+++ src/com/exlab/FirstLesson.java
@@ -3,7 +3,55 @@ package com.exlab;
 public class FirstLesson {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int a = 0;
+		int b = 15;
+		int sum = 1;
+		sum = sum + 5;
+		int index = 0;
+		double c = 9.3;
+		boolean d = false;
+		
+//      simple number
+		for(int i = 0; i<=100; i++) {
+			boolean isPrime = true;
+			
+			for(int j = 2; j < i; j++) {
+				if(i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if(isPrime) {
+				System.out.println(i);
+			}
+		}
+		
+		
+//	    even number
+//		for(int i = 0; i < 15; i++) {
+//			
+//			if((i % 2 == 0) & (i !=0)) {
+//				System.out.println("Yhooo!: " + i);
+//			}
+//			
+//		}
+		
+//		while(a < b) {
+//			a++;
+//			index++;
+//			System.out.println(index + " OK! " + a);
+//		}
+		
+//		if(a != b) {
+//			sum += 5;
+//			System.out.println("OK! " + sum);
+//		} else {
+//			sum = sum - 16;
+//			System.out.println("NOT! " + sum);
+//		}
+		
+//		System.out.println(sum);
 
 	}
 
